cc1002958ad47a801890cd2990b6f59569bedb74
Tamim Hmizi <tamimhmizi7@gmail.com>
2022-12-04 18:13:56 +0100

    project done

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+const pwds = document.querySelectorAll('input[type="password"]');
+pwds.forEach((pwd) => pwd.classList.remove("error"));
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (event.target.elements[4].value != event.target.elements[5].value)
+    pwds.forEach((pwd) => pwd.classList.add("error"));
+    else pwds.forEach((pwd) => pwd.classList.remove("error"));
+});
