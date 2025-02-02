83e85d49d0efd757636c433d6578fa5655590573
Tamim Hmizi <hmizitamim@hotmail.com>
2025-02-02 10:56:22 +0100

    updated designs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sign-Up-Form
+
+this is the sixth project of the odin project course a sign up form made by simple html , css and js.
--- script.js
+++ script.js
@@ -5,5 +5,5 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   if (event.target.elements[4].value != event.target.elements[5].value)
     pwds.forEach((pwd) => pwd.classList.add("error"));
-    else pwds.forEach((pwd) => pwd.classList.remove("error"));
+  else pwds.forEach((pwd) => pwd.classList.remove("error"));
 });
